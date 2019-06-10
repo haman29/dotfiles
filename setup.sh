2fadd3c1f63d653c9d165ef7ee6c5ee9d3f06619
Kyohei Hamada <kyohei.hamada@gmail.com>
2019-06-10 14:45:27 +0900

    fix README

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-DOT_FILES=( .gitconfig .gitignore .irbrc .vimrc .tmux.conf .tmuxinator .dir_colors .pryrc )
-
-for file in ${DOT_FILES[@]}
-do
-    rm $HOME/$file
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
-
+# DOT_FILES=( .gitconfig .gitignore .irbrc .pryrc .tmux.conf .tmuxinator .dir_colors )
+# for file in ${DOT_FILES[@]}
+# do
+#     rm $HOME/$file
+#     ln -s $HOME/dotfiles/$file $HOME/$file
+# done
 # .zshenv
